cd4b146af3b44d345bec0b1cde245747c165c8f7
Gabriel Ramirez <gabriel.rmrz.snchz@gmail.com>
2022-10-19 14:34:02 +0200

    adding wrapper to process in Pisa cluster

---
+++ flatNano/wrapper_pisa_grid.py
@@ -0,0 +1,85 @@
+import os
+import itertools
+import subprocess
+import time
+
+from multiprocessing import Process
+from multiprocessing import Pool
+from write_list_files import storage_list
+
+class task_handler:
+  def __init__(self, file_list=[]):
+    self.file_list = file_list 
+    #self.script_name = 'Resonant_dummy.py'
+    self.script_name = 'Resonant_Rjpsi_v9.py'
+    self.sample_type = 'data'
+  def add_file_name(self, file_name):
+    self.file_list.append(file_name)
+
+  def set_script_name(self, script_name):
+    self.script_name = script_name
+
+  def submit_task(self):
+    '''
+    if (self.input_file == ''):
+      print('ERROR: The input file is missing')
+      return 0
+    elif (self.command == ''):
+      print('ERROR: The command is missing')
+      return 0
+    else:
+    '''
+    for fn in self.file_list:
+      command = 'python ' + self.script_name + ' --' + self.sample_type + '=' + fn
+      print(command)
+    
+      process = subprocess.Popen(command.split(),  stdout=subprocess.PIPE)
+      output, error = process.communicate()
+      print(output)
+
+def pf(file_name):
+  task = task_handler(file_name)
+  task.submit_task()
+
+
+
+
+
+def main():
+  '''
+  handler = task_handler()
+  #handler.set_script_name('Resonant_dummy.py')
+  #handler.set_sample_type('data')
+  handler.add_file_name('test1.txt')
+  handler.add_file_name('test2.txt')
+  handler.add_file_name('test3.txt')
+  #handler.submit_task()
+
+  file_name_prefix = 'test'
+  n_files = 100 
+  file_names = []
+  
+  for n in range(1,n_files+1):
+    file_names.append([file_name_prefix + str(n)+'.txt'])
+  print(file_names)
+    '''
+  batch_size=10
+
+  sl1 = storage_list(request_name='data_Run2018A_UL', dataset_name='MET', date='2022Aug17', job_id='220817_130825', njobs=2940)
+  sl2 = storage_list(request_name='data_Run2018B_UL', dataset_name='MET', date='2022Aug17', job_id='220817_130809', njobs=1425)
+  sl3 = storage_list(request_name='data_Run2018C_UL', dataset_name='MET', date='2022Aug17', job_id='220817_130834', njobs=1351)
+  sl4 = storage_list(request_name='data_Run2018D_UL', dataset_name='MET', date='2022Aug17', job_id='220817_130817', njobs=6284)
+
+  sl1.save_to_files()
+  sl2.save_to_files()
+  sl3.save_to_files()
+  sl4.save_to_files()
+
+  pool = Pool(batch_size)
+  pool.map(pf, sl1.get_file_names())
+
+
+
+
+if __name__ == '__main__':
+  main()
---
+++ flatNano/write_list_files.py
@@ -0,0 +1,65 @@
+import os
+'''
+/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2022Jul06/EGamma/crab_data_Run2018A_UL/220706_123528/0000/RJPsi_data_2022Jul06_14
+
+A -> 2923 -> 220706_123528
+B -> 1409 -> 220706_123519
+C -> 1332 -> 220706_123535
+D -> 6508 -> 220707_140602
+
+'''
+class storage_list:
+  def __init__(self, request_name='data_Run2018A_UL', dataset_name='EGamma', date='2022Jul06', job_id='220706_123528', njobs=2923):
+    self.server_dir = '/gpfs/ddn/srm/cms'
+    self.lfn_dir_base = '/store/user/garamire/crab_job_' + date
+    self.date = date 
+    self.dataset_name=dataset_name
+    self.request_name= request_name
+    self.job_id = job_id
+    self.nfiles_per_job = 10
+    self.njobs = njobs
+    self.full_dir = self.server_dir + self.lfn_dir_base + '/' +self.dataset_name + '/crab_' + self.request_name + '/' + self.job_id
+    self.dir_list = 'list'
+    self.d_paths = {}
+    self.define_names()
+
+  def set_nfiles(self, nfiles_per_job):
+    self.nfiles_per_job = nfiles_per_job
+    self.define_names()
+
+  def define_names(self):
+    #self.file_full_names.clear()
+    file_name = 'list_'+self.dataset_name+'_'+self.request_name
+    a_paths = []
+    for i in range(1, self.njobs+1):
+      a_paths.append("%s/%s/RJPsi_data_%s_%d.root" % (self.full_dir , (str(i//1000)).zfill(4), self.date, i))
+      if (not (i % self.nfiles_per_job) ):
+        path = file_name+'_'+str(i//self.nfiles_per_job )
+        self.d_paths[path] = a_paths.copy()
+        a_paths.clear()
+
+      if (i == self.njobs ):
+        self.d_paths[file_name+'_'+str(1 + i//self.nfiles_per_job)] = a_paths.copy()
+        a_paths.clear()
+  def get_file_names(self):
+    a_dirs = []
+    for key, value in self.d_paths.items():
+      a_dirs.append([self.dir_list+'/'+ key + '.txt'])
+    return a_dirs
+    
+
+  def get_paths(self):
+    return self.d_paths
+
+  def save_to_files(self):
+    try:
+      os.mkdir(self.dir_list)
+    except OSError as error:
+      print(error)
+
+    for key, value in self.d_paths.items():
+      with open(self.dir_list+'/'+ key + '.txt', 'w') as f:
+        for v in value:
+          f.write(v)
+          f.write('\n')
+
